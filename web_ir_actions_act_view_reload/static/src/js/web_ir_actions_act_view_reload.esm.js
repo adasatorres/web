8afc27b70c6b738c45855c1d990bd054da3619a4
Adasat <a.torres@binhex.es>
2023-07-25 13:57:48 +0000

    [16.0][MIG]web_ir_actions_act_view_reload : Migration to 16.0

--- web_ir_actions_act_view_reload/static/src/js/web_ir_actions_act_view_reload.esm.js
+++ web_ir_actions_act_view_reload/static/src/js/web_ir_actions_act_view_reload.esm.js
@@ -6,26 +6,10 @@
 import {registry} from "@web/core/registry";
 const actionHandlersRegistry = registry.category("action_handlers");
 
-function ir_actions_act_view_reload(args) {
-    // Odoo v15 is running in "legacy" mode - some of the JS
-    // is written in Owl, some is still legacy (e.g. controllers
-    // are still written in the old system - hence below hack)
+    function ir_actions_act_view_reload(args) {
+        location.reload();   
 
-    // TODO: for Odoo v16, this will probably need to be re-written in Owl
-    // REF: https://github.com/odoo/odoo/blob/7054fd6beb4f417efa4b22aafe8b935dd6ade123/addons/web/static/src/webclient/actions/action_service.js#L1257-L1267
-
-    const controller = args.env.services.action.currentController;
-    console.log("1");
-    if (controller) {
-        console.log("2");
-        const {__legacy_widget__} = controller.getLocalState();
-        
-            console.log("3");
-            return __legacy_widget__.reload({});
-            
-        
+        console.log(args);
     }
-    return Promise.resolve();
-}
 
 actionHandlersRegistry.add("ir.actions.act_view_reload", ir_actions_act_view_reload);
